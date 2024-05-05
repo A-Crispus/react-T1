fb14e269b5e019fccfc288470190a9dd3db7f8fc
augustrayziss <augustrayziss@gmail.com>
2024-05-05 23:05:57 +0300

    conditioal render

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,38 @@
 function ListGroup() {
     //render the list dynamicly
-    const items =[
-        "New york",
-        "Tokyo",
-        "London",
-        "Paris",
-        "Texas"
+    const items =["New york","Tokyo","London","Paris","Texas"];
+    items [];
 
-    ];
+    //const message =items.length === 0 ? <p>no items found</p> : null;
+    /*const getMessage = () => {
+       return items.length === 0 ? <p>no items found</p> : null
+    this refered after h1
+    } this conditional rendering 
+    {items.length === 0 && <p>no items found</p>}
+    */
+
+
+
+
+//we're repeating so we shud condition render
+ /* if(items.length === 0)
+     return (
+    <>
+      <h1>List</h1>
+      <p>no item found</p>
+    </>);
+    */
+
+
 //curly braces used to render data dynamically
 
   return (
-    /*the empty braces make it ok to add multiple components*/
+    /*the empty braces make it ok to add multiple components
+    
+    */
     <>
     <h1>List</h1>
+    {items.length === 0 && <p>no items found</p> }
     <ul className="list-group">
        {items.map((item) => (
         <li key={item}>{item}</li>
